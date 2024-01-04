7444d628c9103344fdfc92f1426b9978ae85a100
RFDME\yuan.chung <yuan.chung@rfdme.com>
2024-01-04 16:31:16 +0800

    no message

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using p20;
+var s = new Solution();
+var nums = "()[]{}";
+var k = 10;
+var res = s.IsValid(nums);
+Console.WriteLine(res);
